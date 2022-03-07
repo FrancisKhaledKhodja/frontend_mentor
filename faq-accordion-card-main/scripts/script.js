d091a66608fb96743d273944e314822965994f28
Khaled Khodja Francis <francis.khaled-khodja@tdf.fr>
2022-03-07 04:10:22 +0100

    a different approach with the use of details and summary tags

--- faq-accordion-card-main/scripts/script.js
+++ faq-accordion-card-main/scripts/script.js
@@ -1,20 +1,31 @@
 let text = document.querySelector(".faq__questions--answers--card__text")
+console.log(text);
+
+function addSelectedClassName(element) {
+    let ulSelected = element.target.parentElement.parentElement;
+    ["span.question", ".answer", ".icon--arrow"].forEach( element => {
+        ulSelected.querySelectorAll(element).forEach( element => {
+            element.classList.add("selected");
+        });
+    });
+};
+
+function removeSelectedClassName() {
+    ["span.question", ".answer", ".icon--arrow"].forEach( element => {
+        document.querySelectorAll(element).forEach( element => {
+            element.classList.remove("selected");
+        });
+    });
+};
 
 let clickedElement = text.addEventListener("click", function(e) {
-    if (e.target.classList.contains("question")) {
-        // Remove selected className from questions and answers
-        ["span.question", ".answer", ".icon--arrow"].forEach( element => {
-            document.querySelectorAll(element).forEach( element => {
-                element.classList.remove("selected");
-            });
-        });
-
-        // Add selected className on the question and answer selected
-        let ulSelected = e.target.parentElement.parentElement;
-        ["span.question", ".answer", ".icon--arrow"].forEach( element => {
-            ulSelected.querySelectorAll(element).forEach( element => {
-                element.classList.add("selected");
-            });
-        });
+    if (e.target.classList.contains("question") && e.target.classList.contains("selected")) {
+        console.log("Choice 1");
+        removeSelectedClassName();
+    } else if (e.target.classList.contains("question")) {
+        console.log("Choice 2")
+        removeSelectedClassName();
+        addSelectedClassName(e);
     };
 });
+            
